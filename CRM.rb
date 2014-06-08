1cd5cd4a5e4bc1b2db2e74f7cb34e2fcb101cade
theoandtheb <blue.apple@sympatico.ca>
2014-06-08 00:34:27 -0400

    This application does it all.  It slices, it dices, it can cut a tin can as easily as a tomato.  Most importantly, it flows well and looks like it's hard at work.

--- CRM.rb
+++ CRM.rb
@@ -1,6 +1,10 @@
 require_relative 'contact'
 require_relative 'rolodex'
 
+def sleepForSleepsSake
+	sleep 0.1
+end
+
 def intCheck(x,y)
 		$selectr = gets.chomp.to_i
 		unless $selectr.between?(x,y)
@@ -17,7 +21,8 @@ class CRM
 		def initialize(name)
 			@name = name
 			@rolodex = Rolodex.new
-			puts "Welcome to #{name}"
+			print "Welcome to #{name}"
+			sleep 1.4
 		end
 
 		def self.run(name)
@@ -28,24 +33,38 @@ class CRM
 
 
 	def mainMenu
-		print "
-		Main Menu:
-		\n[1] Add new contact
-		\n[2] Modify an existing contact
-		\n[3] Delete a contact
-		\n[4] Display all the contacts
-		\n[5] Display a contact's attributes
-		\n[6] Exit\n\n"
+		print "\n\nMAIN MENU\n"
+		2 * sleepForSleepsSake
+		print "_______________________________\n\n"
+		sleepForSleepsSake
+		print "[1] Add a new contact\n"
+		sleepForSleepsSake
+		print "[2] Modify an existing contact\n"
+		sleepForSleepsSake
+		print "[3] Delete a contact\n"
+		sleepForSleepsSake
+		print "[4] Display all contacts\n"
+		sleepForSleepsSake
+		print "[5] Display an attribute\n"
+		sleepForSleepsSake
+		print "[6] Exit\n\n"
 	end
 
 	def selectOption
-		print "Enter a number: "
+		sleep 0.6
+		print "Please enter the number from the list above and press enter."
+		sleep 0.8
+		print "  I'll wait."
+		3.times { sleepForSleepsSake }
+		print "  =>  "
 		intCheck(1,6)
 		selectr = $selectr
 		call_option(selectr)
 	end
 
 	def call_option(selectr)
+		5.times { sleepForSleepsSake }
+		puts "\e[H\e[2J"
 		case $selectr
 		when 1
 			addNewContact
@@ -63,26 +82,53 @@ class CRM
 	end
 
 	def addNewContact
+		print "ADDING A NEW CONTACT\n______________\n\n"
+		5.times { sleepForSleepsSake }		
  		 print "Enter First Name: "
  		 first_name = gets.chomp
+ 		 sleepForSleepsSake
  		 print "\n\nEnter Last Name: "
  		 last_name = gets.chomp
+ 		 sleepForSleepsSake
  		 print "\n\nEnter Email Address: "
  		 email = gets.chomp
  		 	unless email.include? "@"
  		 		print "\n\nPlease Enter Valid Email Address: "
  			 	email = gets.chomp
 		 	end
+		 	sleepForSleepsSake
  		 print "\n\nEnter a Note: "
  		 note = gets.chomp
  		contact = Contact.new(first_name, last_name, email, note)
  		@rolodex.addContact(contact)
+			4.times { sleepForSleepsSake }
+ 		print "\n\nAdding contact and returning to the main menu.  "
+ 		6.times { sleepForSleepsSake }
+ 		print "Please wait."
+ 		3.times { sleepForSleepsSake }
+ 		print "\n\nProcessing"
+ 		sleep (rand/5)
+ 		print "."
+ 		sleep (rand/5)
+ 		print "." 		
+ 		sleep (rand/5)
+ 		print "."
+ 		 sleep (2*rand)
+ 		print "."
+ 		 sleep (3*rand)
+ 		print "."
+ 		sleep (rand/20)
+ 		print "."
+ 		sleep (rand/20)
+ 		print "."
+ 		4.times { sleepForSleepsSake }
+ 		puts "\e[H\e[2J"
  		 mainMenu
  		 selectOption
 	end
 
 	def findUser
-	 	print "\nPlease specify the contact ID of the contact you wish to modify: " 
+	 	print "\nPlease specify the contact ID would you like to address: " 	
  		modId = gets.chomp.to_i
 		unless modId >= 1000
 			until modId >= 1000
@@ -120,4 +166,6 @@ class CRM
   	end
 end
 
+puts "\e[H\e[2J"
+sleep 1
 CRM.run("G.U.S.S.")
