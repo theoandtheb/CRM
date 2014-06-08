f0377f12d69534f21110deccbd54145b9f12dfbc
theoandtheb <blue.apple@sympatico.ca>
2014-06-07 22:51:14 -0400

    Well, CRM wasn't as complete as I thought it was.  In fact, it wasn't anywhere close.  Can now modify, delete, display all, display single attributes and even exit. Time for bug fixes and beautification.

--- CRM.rb
+++ CRM.rb
@@ -1,6 +1,17 @@
 require_relative 'contact'
 require_relative 'rolodex'
 
+def intCheck(x,y)
+		$selectr = gets.chomp.to_i
+		unless $selectr.between?(x,y)
+			until $selectr.between?(x,y)
+			print "\nLet\'s try that again.\n\nEnter a number: "
+			$selectr = gets.chomp.to_i
+			end
+		end
+end
+
+
 class CRM
 	attr_accessor :name
 		def initialize(name)
@@ -10,7 +21,7 @@ class CRM
 		end
 
 		def self.run(name)
-			crm = new(name)
+			crm = CRM.new(name)
 			crm.mainMenu
 			crm.selectOption
 		end
@@ -21,26 +32,21 @@ class CRM
 		Main Menu:
 		\n[1] Add new contact
 		\n[2] Modify an existing contact
-		\n[3] Delete a contacts
+		\n[3] Delete a contact
 		\n[4] Display all the contacts
-		\n[5] Display a contact's attrinutes
+		\n[5] Display a contact's attributes
 		\n[6] Exit\n\n"
 	end
 
 	def selectOption
 		print "Enter a number: "
-		selectr = gets.chomp.to_i
-		unless selectr.between?(1,6)
-			until selectr.between?(1,6)
-			print "\nLet\'s try that again.\n\nEnter a number: "
-			selectr = gets.chomp.to_i
-			end
-		end
+		intCheck(1,6)
+		selectr = $selectr
 		call_option(selectr)
 	end
 
 	def call_option(selectr)
-		case selectr
+		case $selectr
 		when 1
 			addNewContact
 		when 2
@@ -48,7 +54,7 @@ class CRM
 		when 3
 			deleteContact
 		when 4
-			displayContact
+			displayContacts
 		when 5
 			displayAttribute
 		when 6
@@ -64,15 +70,54 @@ class CRM
  		 print "\n\nEnter Email Address: "
  		 email = gets.chomp
  		 	unless email.include? "@"
- 		 		print "\n\nEnter Valid Email Address: "
+ 		 		print "\n\nPlease Enter Valid Email Address: "
  			 	email = gets.chomp
 		 	end
  		 print "\n\nEnter a Note: "
  		 note = gets.chomp
- 		 contact = Contact.new(first_name, last_name, email, note)
+ 		contact = Contact.new(first_name, last_name, email, note)
+ 		@rolodex.addContact(contact)
  		 mainMenu
  		 selectOption
 	end
+
+	def findUser
+	 	print "\nPlease specify the contact ID of the contact you wish to modify: " 
+ 		modId = gets.chomp.to_i
+		unless modId >= 1000
+			until modId >= 1000
+				 "\nLet\'s try that again.\n\nEnter the contact ID of the contact you wish to modify: : "
+				modId = gets.chomp.to_i
+			end
+		end
+  		modId
+  	end
+
+  	def modifyContact
+  		modId = findUser
+  		@rolodex.modify(modId)
+  		mainMenu
+  		selectOption
+  	end
+
+  	def deleteContact
+  		modId = findUser
+  		@rolodex.delete(modId)
+  		mainMenu
+  		selectOption
+  	end
+
+  	def displayContacts
+  		@rolodex.displayContacts
+  		mainMenu
+  		selectOption
+  	end
+
+  	def displayAttribute
+  		@rolodex.displayAttribute
+  		mainMenu
+  		selectOption
+  	end
 end
 
 CRM.run("G.U.S.S.")
--- rolodex.rb
+++ rolodex.rb
@@ -1,7 +1,8 @@
 class Rolodex
+	attr_accessor :contacts
 	def initialize
-		@contactId = 1000
-		@contacts = []
+		@id = 1000
+		@contacts = Array.new
 	end
 
 	def contacts
@@ -9,9 +10,122 @@ class Rolodex
 	end
 
 	def addContact(contact)
-		contactId = @id
+		contact.id = @id
 		@contacts << contact
 		@id += 1
 	end
 
+	def modify(modId)
+	
+		print "
+		\nPlease select which attribute do you wish to change.
+		Enter the corresponding number and press enter.\n
+		[1] First Name
+		[2] Last Name
+		[3] Email
+		[4] Notes
+		\n\n =>  "
+		attrSel = gets.chomp.to_i
+		unless attrSel.between?(1,4)
+			until attrSel.between?(1,4)
+				print "\nLet\'s try that again.\n\nEnter a number: "
+				attrSel = gets.chomp.to_i
+			end
+		end
+		print "\nWhat would you like to change it to?  "
+		repl = gets.chomp
+		@contacts.each do |contact|
+			if contact.id == modId
+				case attrSel
+					when 1
+						print "\nAre you sure you would like me to replace \"#{contact.first_name}\" with \"#{repl}\"? y/n  "
+						modAnswer = gets.chomp
+						if modAnswer == ("y")
+							contact.first_name = repl
+							print "Your change has been saved successfully."
+						end
+					when 2
+						print "\nAre you sure you would like me to replace \"#{contact.last_name}\" with \"#{repl}? y/n  "
+						modAnswer = gets.chomp
+						if modAnswer == ("y")
+							contact.last_name = repl
+							print "Your change has been saved successfully."
+						end
+					when 3
+						print "\nAre you sure you would like me to replace \"#{contact.email}\" with \"#{repl}? y/n  "
+						modAnswer = gets.chomp
+						if modAnswer == ("y")
+							contact.email = repl
+							print "Your change has been saved successfully."
+						end
+					when 4
+						print "\nAre you sure you would like me to replace \"#{contact.note}\" with \"#{repl}? y/n  "
+						modAnswer = gets.chomp
+						if modAnswer == ("y")
+							contact.note = repl
+							print "Your change has been saved successfully."
+						end
+				end
+			else 
+						print "\nI'm sorry, but there is no contact with that ID.  Please try again."
+			end
+		end
+	end
+
+	def delete(modId)
+		@contacts.each do |contact|
+			if contact.id == modId
+				print "\nAre you sure you would like to delete the profile for #{contact.first_name} #{contact.last_name}?  y/n"
+				delAnswer = gets.chomp
+				if delAnswer == ("y" || "yes")
+					@contacts.delete(contact)
+					print "\nThe selected contact has been successfully deleted."
+				end 
+			else
+				print "\nI'm sorry, but I was not able to locate the contact you requested."
+			end
+		end
+	end
+
+	def displayContacts
+		print "\nYour contacts\n\n"
+		@contacts.each do |contact|
+			print "____________________________________________________________
+			#{contact.first_name} #{contact.last_name} can be reached at #{contact.email}.\n
+			Don't forget, #{contact.note}\n"
+		end
+	end
+
+	def displayAttribute
+		print "\nWhich of these attributes would you like me to display?
+		____________________________________________________________\n
+		[1] First Name
+		[2] Last Name
+		[3] Email
+		[4] Notes
+		\n\n =>  "
+		attrSel = gets.chomp.to_i
+		unless attrSel.between?(1,4)
+			until attrSel.between?(1,4)
+				print "\nLet\'s try that again.\n\nEnter a number: "
+				attrSel = gets.chomp.to_i
+			end
+		end
+		@contacts.each do |contact|
+			case attrSel
+				when 1
+					print "\nFirst Name: #{contact.first_name}"
+				when 2
+					print "\nLast Name: #{contact.last_name}"
+				when 3
+					print "\nEmail: #{contact.email}"
+				when 4
+					print "\nNote: #{contact.note}"
+			end
+		end
+	end
 end
+
+
+
+
